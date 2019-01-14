d6cc9663846580d6e06526a7ea569ce77b870c7f
arturomtm <arturomtm@gmail.com>
2019-01-14 09:25:32 +0100

    fixup! generateRandomBuffer is a util function

--- src/util.js
+++ src/util.js
@@ -1,7 +1,17 @@
+function generateRandomBuffer(len){
+  var packet = new Buffer(len);
+  for (var i = 0; i < len; i++){
+    var byte = Math.floor(Math.random() * 256);
+    packet.writeUInt8(byte, i);
+  }
+  return packet;
+}
+
 function mandatoryParam(name) {
   throw new Error(`The parameter ${param} is mandatory`)
 }
 
 module.exports = {
+  generateRandomBuffer,
   mandatoryParam
 }
