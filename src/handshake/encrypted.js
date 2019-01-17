60d66e5d195e799b2db0594004cf3680f490ffbd
arturomtm <arturomtm@gmail.com>
2019-01-17 13:41:18 +0100

    properly create a server response before sending client second chunk

--- src/handshake/encrypted.js
+++ src/handshake/encrypted.js
@@ -1,12 +1,13 @@
-const { C1, C2 } = require('Handshake') 
-const Handshake = require("./handshake")
+const { C1, C2, S1 } = require('Handshake/dist')
+const Handshake = require("./rfc")
 
 class EncryptedHandshake extends Handshake {
   c1(s0) {
     return C1.create().encode()
   }
   c2(s1) {
-    return C2.create(s1).encode()
+    const _s1 = S1.fromBuffer(s1)
+    return C2.create(_s1).encode()
   }
 }
 
