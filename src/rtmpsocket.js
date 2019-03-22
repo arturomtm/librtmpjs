16670b57f45e33c7f8a6e8fc2f43662aade08acc
arturomtm <arturomtm@gmail.com>
2019-03-22 18:18:17 +0100

    refactor rmpsocket to make it net module style

--- src/rtmpsocket.js
+++ src/rtmpsocket.js
@@ -2,26 +2,25 @@ const net = require('net')
 const { Handshake, EncryptedHandshake } = require('./handshake')
 const StreamFactory = require('./stream_factory')
 
-class RTMPSocket {
-  constructor({host = 'localhost', port = 1935, app, swfUrl, tcUrl, pageUrl} = {}) {
+const connect = async ({host = 'localhost', port = 1935, app, swfUrl, tcUrl, pageUrl} = {}) =>
+  new Promise((resolve, reject) => {
     const socket = net.connect({host, port})
     const handshake = new EncryptedHandshake(socket)
     handshake.once("uninitialized", () => handshake.sendC0C1())
     handshake.once("handshake:done", async () => {
       const streamFactory = new StreamFactory(socket)
       const netConnection = streamFactory.netConnection
-      try {
+      try {
         await netConnection.connect({app, swfUrl, tcUrl, pageUrl})
+        // TODO: maybe, createStream could have a hook to return a NetStream instance
         const [, streamId] = await netConnection.createStream()
-        const netStream = streamFactory.createNetStream(streamId)
-        netStream.on('error', console.log)
-        netStream.on('play:start', console.log)
-        netStream.play('examplestream')
-      } catch(error) {
-        console.log(error)
+        resolve(streamFactory.createNetStream(streamId))
+      } catch(e) {
+        reject(e)
       }
     })
-  }
-}
+  })
 
-module.exports = RTMPSocket
+module.exports = {
+  connect
+}
