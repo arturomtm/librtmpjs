cb6a01fbc48debe52106d2bfe8810129e18fc132
arturomtm <arturomtm@gmail.com>
2019-03-22 18:19:29 +0100

    adapt test to latest changes

--- test/rtmpsocket.spec.js
+++ test/rtmpsocket.spec.js
@@ -1,4 +1,15 @@
-const RTMPSocket = require('../src/rtmpsocket')
+const rtmp = require('../src/rtmp')
 const config = require('./config')
 
-const socket = new RTMPSocket(config)
+async function test() {
+  try {
+    const netStream = await rtmp.connect(config)
+    netStream.on('error', console.log)
+    netStream.on('play:start', console.log)
+    netStream.play(config.playpath)
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+test()
